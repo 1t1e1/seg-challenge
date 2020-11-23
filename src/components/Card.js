4dde9449b0e244094d6ea339c4a8d5bc5b80554c
1t1e1 <1t1ebir@gmail.com>
2020-11-23 12:07:14 +0300

    edit design

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	Card,
 	CardImg,
@@ -9,17 +9,31 @@ import {
 	Button,
 	Row,
 	Col,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
 } from "reactstrap";
+import { FcLike } from "react-icons/fc";
 
 import "./Card.css";
 
 const CustomCard = (props) => {
-	// console.log(props);
+	const [modal, setModal] = useState(false);
+
+	const toggle = () => setModal(!modal);
+	console.log(props);
 	return (
-		<Card className="text-center">
+		<Card className="text-center mt-3">
 			<Row>
 				<Col md="12">
 					<span className="product-indirim">{props.priceText}</span>
+					{props.params.likeCount > 0 && (
+						<div className="kalp-like">
+							<FcLike />
+							<span>+{props.params.likeCount} </span>
+						</div>
+					)}
 					<CardImg
 						className="img-wine"
 						src={props.image}
@@ -28,18 +42,62 @@ const CustomCard = (props) => {
 				</Col>
 				<Col md="12">
 					<CardBody>
-						<CardTitle tag="h4">{props.name}</CardTitle>
-						{/* <CardSubtitle tag="h6" className="mb-2 text-muted">
-							{props.brand}
-						</CardSubtitle> */}
-						<CardText>{props.inStock ? "Var" : "Yok"}</CardText>
-						<Button>
-							{props.priceText + " "}
+						<CardTitle tag="h4">
+							{props.priceText}{" "}
+							<small
+								style={{
+									textDecorationLine: "line-through",
+									textDecorationStyle: "solid",
+									color: "gray",
+								}}
+							>
+								{props.oldPriceText}
+							</small>
+						</CardTitle>
+						<CardSubtitle tag="h6" className="mb-2 text-muted">
+							{props.name}
+						</CardSubtitle>
+						<CardText></CardText>
+						<Button
+							color={props.inStock ? "warning" : "secondary"}
+							disabled={!props.inStock}
+						>
 							Buy
+						</Button>
+						<Button color="info" className="ml-5" onClick={toggle}>
+							View Detail
 						</Button>
 					</CardBody>
 				</Col>
 			</Row>
+			<Modal isOpen={modal} toggle={toggle}>
+				<ModalHeader toggle={toggle}>Detail for {props.productId}</ModalHeader>
+				<ModalBody>
+					<Row>
+						<Col xs="4" md="6">
+							<CardImg
+								className="img-wine"
+								src={props.image}
+								alt="Card image cap"
+							/>
+						</Col>
+						<Col xs="6" md="6">
+							<h4>{props.name}</h4>
+						</Col>
+					</Row>
+				</ModalBody>
+				<ModalFooter>
+					<Button
+						color={props.inStock ? "warning" : "secondary"}
+						onClick={toggle}
+					>
+						Buy One
+					</Button>{" "}
+					<Button color="secondary" onClick={toggle}>
+						Cancel
+					</Button>
+				</ModalFooter>
+			</Modal>
 		</Card>
 	);
 };
